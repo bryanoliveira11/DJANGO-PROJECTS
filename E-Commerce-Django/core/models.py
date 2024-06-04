1c52ac81e98965339a4954d818b86699f5c5daff
bryan <bryan.oliveira12345@gmail.com>
2024-06-04 14:50:59 -0300

    Creating Basic Models

--- E-Commerce-Django/core/models.py
+++ E-Commerce-Django/core/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(
+        max_length=250, default='', blank=True, null=True
+    )
+    image = models.ImageField(
+        upload_to='products/%Y/%m/%d/',
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=11)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=20, default='', blank=True)
+    date = models.DateField(auto_now_add=True, blank=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f'Order N° {self.pk}'
