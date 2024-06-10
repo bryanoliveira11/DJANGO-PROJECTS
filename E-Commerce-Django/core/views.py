26eb185ad7f4eb9cf1e42fe44f23486fafee5289
bryan <bryan.oliveira12345@gmail.com>
2024-06-10 13:43:21 -0300

    Messages

--- E-Commerce-Django/core/views.py
+++ E-Commerce-Django/core/views.py
@@ -45,4 +45,4 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out.')
-    return redirect(reverse('home:home'))
+    return redirect(reverse('home:login'))
