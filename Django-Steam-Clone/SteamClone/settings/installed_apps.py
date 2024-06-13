4b4b8863805189f14bbc8d6582c8dbcf632c0147
bryan <bryan.oliveira12345@gmail.com>
2024-06-13 09:57:15 -0300

    Project Change

---
+++ Django-Steam-Clone/SteamClone/settings/installed_apps.py
@@ -0,0 +1,16 @@
+INSTALLED_APPS = [
+    'store',
+    # django
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # cors headers
+    'corsheaders',
+    # debug toolbar
+    'debug_toolbar',
+    # axes
+    'axes',
+]
--- E-Commerce-Django/ecommerce_app/settings/authentication_backends.py
+++
@@ -1,4 +0,0 @@
-AUTHENTICATION_BACKENDS = [
-    # Django ModelBackend is the default authentication backend.
-    'django.contrib.auth.backends.ModelBackend',
-]
