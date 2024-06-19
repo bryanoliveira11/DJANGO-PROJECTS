c3ba79b377d59f59173804f3d4e17a59b4ce3cf7
bryan <bryan.oliveira12345@gmail.com>
2024-06-19 11:06:47 -0300

    Getting desired data

--- Django-Steam-Clone/games/models.py
+++ Django-Steam-Clone/games/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Games(models.Model):
+    name = models.CharField(max_length=200)
+    steam_appid = models.CharField(max_length=150)
+    short_description = models.CharField(max_length=500)
+    header_image = models.ImageField(
+        upload_to='games/headers/%Y/%m/%d/',
+        blank=True,
+        default='',
+    )
+    minimum_requirements = models.TextField()
+    recommended_requirements = models.TextField()
+    developers = models.CharField(max_length=150)
+    publishers = models.CharField(max_length=150)
+    price = models.CharField(max_length=50)
+    screenshot1 = models.ImageField(
+        upload_to='games/screenshots/%Y/%m/%d/',
+        blank=True,
+        default='',
+    )
+    screenshot2 = models.ImageField(
+        upload_to='games/screenshots/%Y/%m/%d/',
+        blank=True,
+        default='',
+    )
+    screenshot3 = models.ImageField(
+        upload_to='games/screenshots/%Y/%m/%d/',
+        blank=True,
+        default='',
+    )
+    screenshot4 = models.ImageField(
+        upload_to='games/screenshots/%Y/%m/%d/',
+        blank=True,
+        default='',
+    )
+    background_raw = models.ImageField(
+        upload_to='games/backgrounds/%Y/%m/%d/',
+        blank=True,
+        default='',
+    )
--- Django-Steam-Clone/utils/get_details_data.py
+++ Django-Steam-Clone/utils/get_details_data.py
@@ -3,18 +3,48 @@
 import requests
 from steam_popular_ids import ids_list
 
-GAME_ID = ''
-
 
 def get_api_url(id: str):
-    url = f'https://store.steampowered.com/api/appdetails?appids={id}'
+    url = f'https://store.steampowered.com/api/appdetails?appids={
+        id}&l=english'
     return url
 
 
 def get_single_game_data(id: str):
     url = get_api_url(id)
     data = requests.get(url).json()
-    return data or None
+    return data[id]['data']
 
 
-print(get_single_game_data(ids_list[5]))
+def get_necessary_data(data):
+    if data is None:
+        return
+
+    publishers = data.get('publishers')[1]
+
+    if not publishers:
+        publishers = data.get('publishers')[0]
+
+    new_data = {
+        'name': data.get('name'),
+        'steam_appid': data.get('steam_appid'),
+        'short_description': data.get('short_description'),
+        'header_image': data.get('header_image'),
+        'minimum_requirements': data.get('pc_requirements', '').get('minimum'),
+        'recommended_requirements': data.get('pc_requirements', '').get('recommended'),
+        'developers': data.get('developers')[0],
+        'publishers': publishers,
+        'price': data.get('price_overview').get('final_formatted'),
+        'screenshot1': data.get('screenshots')[0].get('path_thumbnail'),
+        'screenshot2': data.get('screenshots')[1].get('path_thumbnail'),
+        'screenshot3': data.get('screenshots')[2].get('path_thumbnail'),
+        'screenshot4': data.get('screenshots')[3].get('path_thumbnail'),
+        'background_raw': data.get('background_raw'),
+    }
+
+    return new_data
+
+
+data = get_single_game_data('1245620')
+new_data = get_necessary_data(data)
+print(new_data)
