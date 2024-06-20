fa7fb020cc8958551df7e2a92b80cc0b537a2f61
bryan <bryan.oliveira12345@gmail.com>
2024-06-20 10:21:58 -0300

    Filling html with actual database data

--- Django-Steam-Clone/store/views.py
+++ Django-Steam-Clone/store/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from games.models import Games
+
 
 class StorePage(View):
     def get(self, *args, **kwargs):
+        all_games = Games.objects.all()
+        slide_games = all_games[:12]
+
         return render(
             self.request,
             'store/pages/store.html',
-            {'title': 'Store'}
+            {
+                'title': 'Store',
+                'slide_games': slide_games,
+            }
         )
