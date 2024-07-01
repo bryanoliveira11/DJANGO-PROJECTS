cb80522b07cf791a661908271ed07707b18613af
bryan <bryan.oliveira12345@gmail.com>
2024-07-01 10:52:54 -0300

    Prices Randomized, games randomized

--- Django-Steam-Clone/store/views.py
+++ Django-Steam-Clone/store/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 from django.views.generic import View
 
@@ -7,7 +9,9 @@ from games.models import Games
 class StorePage(View):
     def get(self, *args, **kwargs):
         all_games = Games.objects.all()
-        slide_games = all_games[:12]
+        rand_start = random.randint(1, len(all_games))
+        rand_price = random.randint(10, 250)
+        slide_games = all_games[rand_start:rand_start + 12]
 
         return render(
             self.request,
@@ -16,5 +20,6 @@ class StorePage(View):
                 'title': 'Store',
                 'slide_games': slide_games,
                 'slide_len': range(len(slide_games) - 1),
+                'rand_price': f'R$ {rand_price},99',
             }
         )
