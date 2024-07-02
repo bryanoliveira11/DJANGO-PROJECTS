3e38931fdeb04f6a8b45f6c5b5bbef7ea4a53aee
bryan <bryan.oliveira12345@gmail.com>
2024-07-02 10:07:25 -0300

    Corrections

--- Django-Steam-Clone/store/views.py
+++ Django-Steam-Clone/store/views.py
@@ -7,10 +7,12 @@ from games.models import Games
 
 
 class StorePage(View):
+    def get_rand_start(self, games) -> int:
+        return random.randint(1, (len(games) - 12))
+
     def get(self, *args, **kwargs):
         all_games = Games.objects.all()
-        rand_start = random.randint(1, (len(all_games) - 12))
-        rand_price = random.randint(10, 250)
+        rand_start = self.get_rand_start(all_games)
         slide_games = all_games[rand_start:rand_start + 12]
 
         return render(
@@ -20,6 +22,5 @@ class StorePage(View):
                 'title': 'Store',
                 'slide_games': slide_games,
                 'slide_len': range(1, (len(slide_games) + 1)),
-                'rand_price': f'R$ {rand_price},99',
             }
         )
