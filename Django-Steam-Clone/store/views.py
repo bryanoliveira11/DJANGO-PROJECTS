aa0b3bda6e50ea96c824ad2be07008f47c4d504f
bryan <bryan.oliveira12345@gmail.com>
2024-07-04 12:57:47 -0300

    getting the new slides length for the sales content

--- Django-Steam-Clone/store/views.py
+++ Django-Steam-Clone/store/views.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 from django.db.models.manager import BaseManager
@@ -19,12 +20,33 @@ class StorePage(View):
             rand_games.append(game)
         return rand_games
 
+    def get_slide_games(
+        self, games: BaseManager[Games], is_sale=False
+    ) -> tuple[BaseManager[Games], bool]:
+        rand_start = self.get_rand_start(games)
+
+        # off sale, games = 12 in total, 12 slides
+        if not is_sale:
+            slide_games = games[rand_start:rand_start + 12]
+            return slide_games, is_sale
+
+        # on sale, games = 24 in total, 3 per slide, 8 slides
+        slide_games = games[rand_start: rand_start + 24]
+        return slide_games, is_sale
+
+    def get_slide_length(self, is_sale: bool, slide_games: BaseManager[Games]):
+        slide_len = range(1, (len(slide_games) + 1))
+
+        if is_sale:
+            slide_len = range(1, math.floor((len(slide_games) / 3)) + 1)
+
+        return slide_len
+
     def get(self, *args, **kwargs):
         header, background, is_video = get_store_visual_assets()
         all_games = Games.objects.all()
-        rand_start = self.get_rand_start(all_games)
-        slide_games = all_games[rand_start:rand_start + 12]
         rand_games = self.get_rand_games(5, all_games)
+        slide_games, is_sale = self.get_slide_games(all_games, True)
 
         return render(
             self.request,
@@ -34,8 +56,9 @@ class StorePage(View):
                 'header': header,
                 'background': background,
                 'is_video': is_video,
+                'is_sale': is_sale,
                 'slide_games': slide_games,
                 'rand_games': rand_games,
-                'slide_len': range(1, (len(slide_games) + 1)),
+                'slide_len': self.get_slide_length(is_sale, slide_games),
             }
         )
